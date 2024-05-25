a3a71f7d384f4bde32d075286b370c4dcaf1b40e
glesi <glesi.mayandy@gmail.com>
2024-05-24 20:53:02 -0600

    Se agrega carpeta js para archivos con codigo javascript.

--- js/acciones.js
+++ js/acciones.js
@@ -1,1 +1,16 @@
-console.log ("Hola Mundo")
+function realizarDeposito(){
+    Swal.fire("SweetAlert2 is working!");
+}
+
+const salir = () => {
+    Swal.fire({
+        title: "Realmente desea salir del sistema?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Si"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                location.href = "login.html";
+            }
+        });
+}
