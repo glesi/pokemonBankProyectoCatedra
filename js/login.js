ce5f6bf89d1db183e2d3f0132e2e7095b3ec7257
Parada2024 <151289228+Parada2024@users.noreply.github.com>
2024-06-01 16:51:18 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,102 +1,81 @@
-document.addEventListener("DOMContentLoaded", function () {
-  //Variables para guardar los valores ingresados
-  const usuarioInput = document.getElementById("usuarioInput");
-  const passwordInput = document.getElementById("passwordInput");
-  const loginButton = document.getElementById("loginButton");
-  const registerButton = document.getElementById("registerButton");
-  const errorMessage = document.getElementById("errorMessage");
-  const errorMessageUsuario = document.getElementById("errorMessageUsuario");
-  errorMessage.innerHTML = "";
-  errorMessageUsuario.innerHTML = "";
-  usuarioInput.parentElement.removeAttribute("id");
-  passwordInput.parentElement.removeAttribute("id");
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pokemon Bank - Login</title>
+    <!--Bootstrap css-->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <!--Bootstrap JS-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+    <!--Estilos Internos-->
+    <link rel="stylesheet" href="./css/login.css" />
+    <!--Iconos-->
+    <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <!--Cuadro para formulario de Ingreso-->
+    <div class="Whole">
+      <div class="login">
+        <!--Formulario para Ingreso-->
+        <form id="loginForm">
+          <div class="logo">
+            <img src="./img/Pokemon-Logo.png" alt="Logo-Pokemon" />
+          </div>
+          <div class="titulos">
+            <h1>¡Bienvenido!</h1>
+            <h2>Iniciar Sesión</h2>
+          </div>
+          <!--Ingreso de Usuario y Contraseña-->
+          
+            <div class="input-box" id="usuario-box">
+              <input
+                type="text"
+                id="usuarioInput"
+                placeholder="Usuario"
+                autocomplete="off"
+              />
+            </div>
+            <div class="input-box" id="password-box">
+              <input
+                type="password"
+                id="passwordInput"
+                placeholder="Contraseña"
+                autocomplete="off"
+              />
+            </div>
+          
+          
 
-  // Definir las restricciones de validación
-  const constraints = {
-    contraseña: {
-      presence: {
-        allowEmpty: false,
-        message: "no puede estar vacío",
-      },
-      length: {
-        is: 4,
-        message: " debe tener exactamente 4 dígitos",
-      },
-      format: {
-        pattern: "[0-9]+",
-        message: " solo puede contener números",
-      },
-    },
-    usuario: {
-      presence: {
-        allowEmpty: false,
-        message: "no puede estar vacío",
-      },
-    },
-  };
-
-  //Boton para poder ingresar
-  loginButton.addEventListener("click", function (event) {
-    // Evita el envío del formulario y la recarga de la página
-    event.preventDefault();
-    errorMessage.innerHTML = "";
-    errorMessageUsuario.innerHTML = "";
-    usuarioInput.parentElement.removeAttribute("id");
-    passwordInput.parentElement.removeAttribute("id");
-    // Recoger los valores del formulario
-    const formData = {
-      usuario: usuarioInput.value.trim(),
-      contraseña: passwordInput.value,
-    };
-
-    // Validar el formulario
-    const errors = validate(formData, constraints);
-
-    if (errors) {
-      if (errors.usuario) {
-        errorMessageUsuario.innerHTML = errors.usuario.join(",  ");
-        usuarioInput.parentElement.setAttribute("id", "usuario-error");
-      } else {
-        errorMessageUsuario.innerHTML = "";
-      }
-
-      if (errors.contraseña) {
-        errorMessage.innerHTML = errors.contraseña.join(",  ");
-        passwordInput.parentElement.setAttribute("id", "password-error");
-      } else {
-        errorMessage.innerHTML = "";
-      }
-    } else {
-      const usuario = usuarioInput.value.trim();
-      const contraseña = passwordInput.value.trim();
-
-      if (usuario === "admin" && contraseña === "1234") {
-        // Datos del usuario
-        const user = {
-          name: "Ash Ketchum",
-          account: "0987654321",
-          balance: 500.0,
-        };
-        // Almacenar datos en LocalStorage
-        //se coniverte el objeto a string
-        localStorage.setItem("user", JSON.stringify(user));
-        // Redirigir a la página de transacciones
-        window.location.href = "transacciones.html";
-      } else {
-        swal({
-          icon: "error",
-          title: "Error",
-          text: "Usuario o PIN incorrecto.",
-        });
-      }
-    }
-  });
-
-  //Boton para abrir ventana de registro
-  registerButton.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    // Redirigir a usuarios.html
-    window.location.href = "usuarios.html";
-  });
-});
+          <p id="errorMessage" style="color: red"></p>
+          <p id="errorMessageUsuario" style="color: red"></p>
+          <!--Botones para ingresar y registrar-->
+          <div class="btn-group">
+            <button type="button" class="btn btn-inicio" id="loginButton">
+              Iniciar Sesión
+            </button>
+            <button type="button" class="btn btn-registro" id="registerButton">
+              Registrarse
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!--Acciones Internas JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="./js/login.js"></script>
+  </body>
+</html>
